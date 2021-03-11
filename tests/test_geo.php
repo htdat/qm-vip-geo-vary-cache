280375362d93282f0eccb93aa560a7a18a4c508a
Dat Hoang <htdat@users.noreply.github.com>
2021-03-11 20:47:17 +0700

    Convert indentations to tab

--- tests/test_geo.php
+++ tests/test_geo.php
@@ -2,7 +2,7 @@
 
 class QM_VIP_Geo_Test extends WP_UnitTestCase {
 
-    public function setUp() {
+	public function setUp() {
 		parent::setUp();  
 		
 		$admin = $this->factory->user->create_and_get( array(
@@ -15,43 +15,43 @@ class QM_VIP_Geo_Test extends WP_UnitTestCase {
 
 		wp_set_current_user( $admin->ID );
 
-        if ( class_exists( 'VIP_Go_Geo_Uniques' ) ) {
-            VIP_Go_Geo_Uniques::set_default_location( 'US' );
-            VIP_Go_Geo_Uniques::add_location( 'GB' );
-            VIP_Go_Geo_Uniques::add_location( 'VN' );
-            VIP_Go_Geo_Uniques::add_location( 'SG' );
-            VIP_Go_Geo_Uniques::add_location( 'JP' );
-            VIP_Go_Geo_Uniques::add_location( 'HK' );
-            VIP_Go_Geo_Uniques::add_location( 'ES' );
-        }
-        
-        define( 'WP_USE_THEMES', true );
+		if ( class_exists( 'VIP_Go_Geo_Uniques' ) ) {
+			VIP_Go_Geo_Uniques::set_default_location( 'US' );
+			VIP_Go_Geo_Uniques::add_location( 'GB' );
+			VIP_Go_Geo_Uniques::add_location( 'VN' );
+			VIP_Go_Geo_Uniques::add_location( 'SG' );
+			VIP_Go_Geo_Uniques::add_location( 'JP' );
+			VIP_Go_Geo_Uniques::add_location( 'HK' );
+			VIP_Go_Geo_Uniques::add_location( 'ES' );
+		}
+		
+		define( 'WP_USE_THEMES', true );
 
 		if ( ! isset( $_SERVER['REQUEST_METHOD'] ) ) {
 			$_SERVER['REQUEST_METHOD'] = 'GET';
 		}
-        remove_action( 'template_redirect', 'redirect_canonical' );
+		remove_action( 'template_redirect', 'redirect_canonical' );
 
-        require_once ABSPATH . WPINC . '/template-loader.php';
+		require_once ABSPATH . WPINC . '/template-loader.php';
 
-        $this->html = QM_Dispatchers::get( 'html' );
+		$this->html = QM_Dispatchers::get( 'html' );
 
 		$this->html->init();
 
 	}
 
-    public function test_just_a_test() {         
-        $this->assertTrue ( class_exists( 'VIP_Go_Geo_Uniques' ) ); 
-        $this->assertTrue ( class_exists( 'QueryMonitor' ) ); 
+	public function test_just_a_test() {         
+		$this->assertTrue ( class_exists( 'VIP_Go_Geo_Uniques' ) ); 
+		$this->assertTrue ( class_exists( 'QueryMonitor' ) ); 
 
-        $this->go_to( 'wp-admin' );
+		$this->go_to( 'wp-admin' );
 
-        ob_start();
+		ob_start();
 		$this->html->dispatch();
 		$output = ob_get_flush();
-        $needle = 'VIP Geo Location';
+		$needle = 'VIP Geo Location';
 
-        $this->assertStringContainsString( $needle, $output );
+		$this->assertStringContainsString( $needle, $output );
 
-    }
+	}
 }
--- tests/test_vary_cache.php
+++ tests/test_vary_cache.php
@@ -2,11 +2,11 @@
 
 class Vary_Cache_Test extends WP_UnitTestCase {
 
-    public function test_just_a_test() {
-        $a = 'abc';
-        $b = 'abc'; 
+	public function test_just_a_test() {
+		$a = 'abc';
+		$b = 'abc'; 
 		$this->assertEquals( $a, $b );
-        $this->assertTrue ( class_exists( '\Automattic\VIP\Cache\Vary_Cache' ) ); 
+		$this->assertTrue ( class_exists( '\Automattic\VIP\Cache\Vary_Cache' ) ); 
 
-    }
+	}
 }
