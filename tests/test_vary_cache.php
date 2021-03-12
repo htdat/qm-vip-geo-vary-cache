a64036939c7f13daf96faad916d9cd2be5d6bcd4
Dat Hoang <htdat@users.noreply.github.com>
2021-03-12 14:23:05 +0700

    Add tests for Vary Cache

--- tests/test_vary_cache.php
+++ tests/test_vary_cache.php
@@ -1,12 +1,51 @@
-<?php 
+<?php
 
-class Vary_Cache_Test extends WP_UnitTestCase {
+use Automattic\VIP\Cache\Vary_Cache;
 
-	public function test_just_a_test() {
-		$a = 'abc';
-		$b = 'abc'; 
-		$this->assertEquals( $a, $b );
-		$this->assertTrue ( class_exists( '\Automattic\VIP\Cache\Vary_Cache' ) ); 
+class QM_VIP_Cache_Test extends QM_VIP_Base_Test {
 
+	public function setUp() {
+		Vary_Cache::unload(); 
+
+		parent::setUp(); 
+
+		$this->assertTrue( class_exists( '\Automattic\VIP\Cache\Vary_Cache' ) ); 
+		$this->assertTrue( class_exists( 'QueryMonitor' ) ); 
+	
+	}
+
+	public function test_output_include_group_name_without_any_value() {
+		Vary_Cache::register_group( 'test-group-1' );
+
+		ob_start();
+		$this->html->dispatch();
+		$output = ob_get_flush();
+
+		$this->assertStringContainsString( 'test-group-1', $output );
+	}
+
+	public function test_output_include_group_name_with_value() {
+		// Set cookie value to mock a segment 
+		$_COOKIE = [
+			'vip-go-seg' => 'test2_--_value2',
+		];
+
+		Vary_Cache::register_group( 'test2' );
+		Vary_Cache::parse_cookies();
+
+		ob_start();
+		$this->html->dispatch();
+		$output = ob_get_flush();
+
+		$this->assertStringContainsString( 'test2', $output );
+		$this->assertStringContainsString( 'value2', $output );
+	}
+
+	public function test_output_without_any_group() {
+		ob_start();
+		$this->html->dispatch();
+		$output = ob_get_flush();
+
+		$this->assertStringContainsString( 'No group is defined', $output );
 	}
 }
